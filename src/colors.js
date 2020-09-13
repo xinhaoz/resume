94099a8314a64e9cac19e0194e37d7b6ea3d0010
Xin Hao Zhang <xinhao.zhang98@gmail.com>
2020-09-13 19:23:13 -0400

    Add current work term

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,7 @@
-export const highlight = "#80bfff";
+// export const highlight = "#80bfff"; // blue
+export const highlight = "#ff9999"; // red
 
 export const deemphasis = "#bebebe";
 
-export const emphasis = "#0073e6";
+// export const emphasis = "#0073e6";
+export const emphasis = "#ff6666";
